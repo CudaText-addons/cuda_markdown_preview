e684ffd73b7012016a1b624ae79dc50faa299a29
Alexey-t <support@uvviewsoft.com>
2019-09-29 16:19:25 +0300

    disabled GFM module

--- cuda_markdown_options.py
+++ cuda_markdown_options.py
@@ -1,11 +1,4 @@
-from mdx_gfm import GithubFlavoredMarkdownExtension
-from gfm import AutolinkExtension, AutomailExtension, TaskListExtension
-
 ext = [
   'markdown.extensions.extra',
   'markdown.extensions.codehilite',
-  GithubFlavoredMarkdownExtension(),
-  AutolinkExtension(), 
-  AutomailExtension(), 
-  TaskListExtension(), 
   ]
